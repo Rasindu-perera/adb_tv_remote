7f7cce8e34f063901434d2094916114fd6f9ff6c
W.K.R.P Perera <wkrpperera@students.nsbm.ac.lk>
2026-07-31 11:31:19 +0530

    app name chenge and logo add

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'ADB TV Remote',
+      title: 'KWR TV Remote',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
